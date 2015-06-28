d8a44ac0846d39efc10d1cae8e7a6cb0f878a88c
JeffTanner <jjtanner7@gmail.com>
2015-06-27 22:09:06 -0400

    Added data for starter category and subcategory csv files, updated the createCsv method to require an array of arrays

--- setup.py
+++ setup.py
@@ -10,19 +10,42 @@ dbFolder = ".db"
 dbName = "transactions.db"
 bankStateFolder = "Bank_Statements"
 statementSetupFolder = "setup"
+categories = [
+    ['id','category'],
+    ['1','Gas'],
+    ['2','Food'],
+    ['3','living expenses'],
+    ['4','Clothing'],
+    ['5','Other'],
+]
+subcategories = [
+    ['sub_cat_id','cat_id','sub_cat'],
+    ['1','2','grocery'],
+    ['2','2','eating out'],
+    ['3','3','insurance'],
+    ['4','3','utilities'],
+    ['5','3','rent'],
+    ['6','3','other']
+]
 
 def createCsv(fileName, content):
     with open(fileName, 'wb') as csvfile:
         csvWriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
-        csvWriter.writerow(content)
-        del csvWriter
+        for row in content:
+            csvWriter.writerow(row)
+#        del csvWriter
+
+
 
 def createFolderStructure(basePath):
     curPath = os.path.join(basePath, appFolder, expFolder, bankStateFolder, statementSetupFolder)
     if not os.path.exists(curPath):
         os.makedirs(curPath)
         os.chdir(curPath)
-        createCsv('banks.csv', ['bank prefix','Date','Check Number','Description','Debit','Credit'])
+        createCsv('banks.csv', [['bank prefix','Date','Check Number','Description','Debit','Credit']])
+        createCsv('categories.csv', categories)
+        createCsv('subcategories.csv', subcategories)
+        createCsv('category-transaction_matching.csv', [['keyword','category','subcategory'],['gas', '5', '3']])
     curPath = os.path.join(basePath, appFolder, expFolder, dbFolder)
     if not os.path.exists(curPath):
         os.makedirs(curPath)
@@ -32,6 +55,3 @@ def createFolderStructure(basePath):
     elif not os.path.exists(os.path.join(basePath, appFolder, expFolder, dbFolder, dbName)):
         os.chdir(curPath)
         conn = sqlite3.connect(dbName)
-        conn.commit()
-        
-#def setupDatabase(filePath):
