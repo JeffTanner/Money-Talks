3768c54e59a94d1499094e186ef2079aeb2ddff3
JeffTanner <jjtanner7@gmail.com>
2015-06-27 22:26:55 -0400

    Converted the category and subcategory tables to tuples, updated the csv writer to take in a header param so the tuples could also be used to write the db tables. Started writing the setupDatabase function

--- setup.py
+++ setup.py
@@ -11,47 +11,54 @@ dbName = "transactions.db"
 bankStateFolder = "Bank_Statements"
 statementSetupFolder = "setup"
 categories = [
-    ['id','category'],
-    ['1','Gas'],
-    ['2','Food'],
-    ['3','living expenses'],
-    ['4','Clothing'],
-    ['5','Other'],
+    ('1','Gas'),
+    ('2','Food'),
+    ('3','living expenses'),
+    ('4','Clothing'),
+    ('5','Other'),
 ]
 subcategories = [
-    ['sub_cat_id','cat_id','sub_cat'],
-    ['1','2','grocery'],
-    ['2','2','eating out'],
-    ['3','3','insurance'],
-    ['4','3','utilities'],
-    ['5','3','rent'],
-    ['6','3','other']
+    ('1','2','grocery'),
+    ('2','2','eating out'),
+    ('3','3','insurance'),
+    ('4','3','utilities'),
+    ('5','3','rent'),
+    ('6','3','other')
 ]
 
-def createCsv(fileName, content):
+def createCsv(fileName, header, content):
     with open(fileName, 'wb') as csvfile:
         csvWriter = csv.writer(csvfile, delimiter=',', quoting=csv.QUOTE_NONNUMERIC)
+        csvWriter.writerow(header)
         for row in content:
             csvWriter.writerow(row)
 #        del csvWriter
 
-
+def setupDatabase():
+    #connect to/create the database
+    dbConn = sqlite3.connect(dbName)
+    dbCur = dbConn.cursor()
+    
+    # add all appropriate tables
+    dbCur.execute('''CREATE TABLE purchases (id INTEGER PRIMARY KEY, date text, description text, debit real, credit real)''')
+    
+    dbConn.commit()
+    dbConn.close()
 
 def createFolderStructure(basePath):
     curPath = os.path.join(basePath, appFolder, expFolder, bankStateFolder, statementSetupFolder)
     if not os.path.exists(curPath):
         os.makedirs(curPath)
         os.chdir(curPath)
-        createCsv('banks.csv', [['bank prefix','Date','Check Number','Description','Debit','Credit']])
-        createCsv('categories.csv', categories)
-        createCsv('subcategories.csv', subcategories)
-        createCsv('category-transaction_matching.csv', [['keyword','category','subcategory'],['gas', '5', '3']])
+        createCsv('banks.csv', ['bank prefix','Date','Check Number','Description','Debit','Credit'], [])
+        createCsv('categories.csv', ['id','category'], categories)
+        createCsv('subcategories.csv', ['sub_cat_id','cat_id','sub_cat'], subcategories)
+        createCsv('category-transaction_matching.csv', ['keyword','category','subcategory'],[['gas', '5', '3']])
+    
     curPath = os.path.join(basePath, appFolder, expFolder, dbFolder)
     if not os.path.exists(curPath):
         os.makedirs(curPath)
-        os.chdir(curPath)
-        conn = sqlite3.connect(dbName)
-        
-    elif not os.path.exists(os.path.join(basePath, appFolder, expFolder, dbFolder, dbName)):
-        os.chdir(curPath)
-        conn = sqlite3.connect(dbName)
+    
+    os.chdir(curPath)
+    if not os.path.exists(os.path.join(basePath, appFolder, expFolder, dbFolder, dbName)):
+        setupDatabase()
