c7d2f8c8afc49c1f22259bb04d963d2a5cb8d00c
JeffTanner <jjtanner7@gmail.com>
2015-06-28 00:56:57 -0400

    Wrote the code to pull in the bank statements and add them into the database without duplicating entries. Haven't implemented the categorization of the entries yet

--- money_talks.py
+++ money_talks.py
@@ -9,6 +9,7 @@ __date__ = "$Jun 27, 2015 7:45:55 PM$"
 dbConn = ""
 dbCur = ""
 
+
 def readInCsv(csvFilePath):
     rows = []
     with open(csvFilePath, 'rb') as csvFile:
@@ -25,15 +26,55 @@ def readInCsv(csvFilePath):
                 rows.append(row)
     return rows
 
+def processBankStatement(data, schema):
+    global dbConn, dbCur
+    entries = []
+    for entry in data:
+        curEntry = []
+        
+        # Setup the date of the current transaction
+        datePieces = entry[(int(schema[1])-1)].split('/');
+        curEntry.append(datePieces[2]) # Adding the year
+        curEntry.append(datePieces[0]) # Adding the month
+        curEntry.append(datePieces[1]) # Adding the day
+        
+        # Add the check number
+        curEntry.append(entry[(int(schema[2])-1)])
+        
+        # Add the description
+        curEntry.append(entry[(int(schema[3])-1)])
+        
+        # Add the debit
+        curEntry.append(entry[(int(schema[4])-1)])
+        
+        # Add the credit
+        curEntry.append(entry[(int(schema[5])-1)])
+        
+        # Add the curEntry to the entries list
+        entries.append(curEntry)
+        
+        #TODO:: CATEGORIZE THE TRANSACTIONS
+        
+        for row in dbCur.execute("SELECT count(*) FROM purchases WHERE year=? AND month=? AND day=? AND check_num=? AND description=? AND debit=? AND credit=?", curEntry):
+            print row[0]
+            if row[0] == 0:
+                dbCur.execute("INSERT INTO purchases (year, month, day, check_num, description, debit, credit) VALUES (" + curEntry[0] + ", " + curEntry[1] + ", " + curEntry[2] + ", " + curEntry[3] + ", \"" + curEntry[4] + "\", " + curEntry[5] + ", " + curEntry[6] +")")
+                dbConn.commit()
+                
+#        dbConn.commit()
+#        print dbCur.rowcount
+#    dbCur.executemany("INSERT INTO purchases VALUES (year, month, day, description, debit, credit)", entries)
+#    dbConn.commit()
+
 def setupDatabase():
     global dbConn, dbCur
-#    dbConn = sqlite3.connect('transactions.db')
-#    dbCur = dbConn.cursor()
     categories = readInCsv('categories.csv')
     subcategories = readInCsv('subcategories.csv')
     matches = readInCsv('category-transaction_matching.csv')
     os.chdir('../../.db')
     setupMoney.setupDatabase(categories, subcategories, matches)
+    dbConn = sqlite3.connect('transactions.db')
+    dbCur = dbConn.cursor()
 
 parser = OptionParser()
 parser.add_option("-s", "--setup", dest="setupPath", help="Setup the folder structure for the Money_Talks application", metavar="SETUP")
@@ -55,14 +96,16 @@ else:
     print "Reading in your bank statements . . . "
     # Read in the bank statement and start categorizing
     bankSchema = readInCsv('banks.csv')
-    print bankSchema
     os.chdir('../')
     statements = [ f for f in listdir(os.getcwd()) if isfile(join(os.getcwd(),f)) ]
     
     for indvStat in statements:
         for bank in bankSchema:
             if indvStat.lower().find(bank[0].lower()) > -1:
-                print indvStat + " found " + bank[0]
-                break
-    print statements
+                statementCont = readInCsv(indvStat)
+                # Now iterate through the statement and add it to the database
+                processBankStatement(statementCont, bank)
+    
     # Prompt user if it doesn't know where to put something
+    
+    print "Money Talks is Complete"
